dc7cb542427c04c7ce2567ef9b1e69e25f0eb9df
zimozhai <2372345481@qq.com>
2022-10-20 22:40:27 +0800

    Opening passed

--- Opening.cpp
+++ Opening.cpp
@@ -12,14 +12,43 @@ int main(){
 	//腐蚀和膨胀运算需要结构元先反射，本题反射前后并无变化故省略 
 	vector<vector<int>> tmp(hn,vector<int>(wm,0)) ;
 	vector<vector<int>> src(hn,vector<int>(wm,0)) ;//待处理灰度二值图像 
+	vector<vector<int>> out(hn,vector<int>(wm,0)) ;
+	
 	for(int i=0;i<hn;i++){
 		for(int j=0;j<wm;j++){
 			cin>>src[i][j];
 			tmp[i][j]=0;
 		}
 	}
+	//腐蚀运算
+	for(int i=0;i<hn;i++){
+		for(int j=0;j<wm;j++){
+			
+			for(int m=-hf/2;m<=hf/2;m++){
+				for(int n=-wf/2;n<=wf/2;n++){
+					if((i+m)<0||(n+j)<0||(i+m)>=hn||(j+n)>=wm||!src[i+m][j+n]) goto A; 
+				}	
+			}
+			tmp[i][j]=255;
+			A:;
+		} 
+	} 
 	
 	
+	//膨胀运算
+	for(int i=0;i<hn;i++){
+		for(int j=0;j<wm;j++){
+			if(!tmp[i][j]) continue;
+			for(int m=-hf/2;m<=hf/2;m++){
+				for(int n=-wf/2;n<=wf/2;n++){
+					if((i+m)<0||(n+j)<0||(i+m)>=hn||(j+n)>=wm) continue; 
+					out[i+m][j+n]=255;
+				}	
+			}
+		} 
+	}
+	
+	/* 
 	for(int i=hf/2;i<hn-hf/2;i++){
 		for(int j=wf/2;j<wm-wf/2;j++){
 			
@@ -39,11 +68,12 @@ int main(){
 			A:;
 		}
 	}
+	*/
 	
 	for(int i=0;i<hn;i++){
 		for(int j=0;j<wm-1;j++){
-			cout<<tmp[i][j]<<" ";
+			cout<<out[i][j]<<" ";
 		}
-		cout<<tmp[i][wm-1]<<endl;
+		cout<<out[i][wm-1]<<endl;
 	}
 } 
